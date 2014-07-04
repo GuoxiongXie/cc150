0731516c2508d23eeb19140e37423b92e13a5fe2
Guoxiong Xie <felixxie1218@gmail.com>
2014-07-04 11:29:14 -0700

    BinaryTree skeleton

--- ch4TreesAndGraphs/BinaryTreeImplementation/BinaryTree.java
+++ ch4TreesAndGraphs/BinaryTreeImplementation/BinaryTree.java
@@ -2,6 +2,57 @@
    together with an inner class binary node.
    Reference: Yellow artichoke book P661~662 */
 
-public class BinaryTree<Object> {
+public class BinaryTree<T> {
+    private BinaryNode<T> root;
 
+    //constructors
+    public BinaryTree(){
+	root = null;
+    }
+    public BinaryTree(T rootItem){
+	BinaryNode<T> rootNode = new BinaryNode<T>(rootItem, null, null);
+	root = rootNode;
+    }
+    
+    public BinaryNode<T> getRoot(){
+	return root;
+    }
+    public int size(){
+	return BinaryNode.size(root);
+    }
+    public int height(){
+	return BinaryNode.height(root);
+    }
+    
+
+    //tree traversal
+    public void printPreOrder(){
+	if(root != null)
+	    root.printPreOrder();
+	else
+	    System.out.println("The binary tree is empty");
+    }
+    public void printInOrder(){
+	if(root != null)
+	    root.printInOrder();
+	else
+	    System.out.println("The binary tree is empty");
+    }
+    public void printPostOrder(){
+	if (root != null)
+	    root.printPostOrder();
+	else
+	    System.out.println("The binary tree is empty");
+    }
+
+    public void makeEmpty(){
+	root = null;
+    }
+    public boolean isEmpty(){
+	return root == null;
+    }
+
+    public void merge(T rootItem, BinaryTree<T> binaryTree1, BinaryTree<T> binaryTree2){
+	//TODO
+    }
 }
