baeb5c5a9f5543a485e9b044d6dfd9b7fe1583cf
Guoxiong Xie <felixxie1218@gmail.com>
2014-07-03 17:54:04 -0700

    added binary tree implementation

---
+++ ch4TreesAndGraphs/BinaryTreeImplementation/BinaryTree.java
@@ -0,0 +1,7 @@
+/* This is an implementation of a binary tree
+   together with an inner class binary node.
+   Reference: Yellow artichoke book P661~662 */
+
+public class BinaryTree<Object> {
+
+}
